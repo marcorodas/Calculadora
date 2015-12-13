085b8ccae3b4bc2a1e60ff7def22b7bf95b4c3af
marco.rodas <marco.rodas@pucp.edu.pe>
2015-12-13 02:45:37 -0500

    Changes2

---
+++ app/src/main/java/com/as_supportpe/calculadora/Calculator.java
@@ -0,0 +1,33 @@
+package com.as_supportpe.calculadora;
+
+/**
+ * Created by Administrator on 13/12/2015.
+ */
+
+import org.boris.expr.*;
+import org.boris.expr.parser.ExprParser;
+
+public class Calculator {
+/*
+SimpleEvaluationContext context = new SimpleEvaluationContext();
+        System.out.println("Expr Evaluator v1.0");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            try {
+                System.out.print(">");
+                String line = br.readLine();
+                if (line == null)
+                    break;
+                Expr e = ExprParser.parse(line);
+                Exprs.toUpperCase(e);
+                if (e instanceof ExprEvaluatable) {
+                    e = ((ExprEvaluatable) e).evaluate(context);
+                }
+                System.out.println(e);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+*/
+
+}
